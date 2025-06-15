c17655e30db9b06ff8ff76365cefc060eeef78d1
Muhammad Aulia Utama <75517096+muhauliautama@users.noreply.github.com>
2025-06-15 15:42:50 +0700

    Fix development server startup

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_DEV: string
+  readonly VITE_ADMIN_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
